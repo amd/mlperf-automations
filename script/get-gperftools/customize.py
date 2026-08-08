329006031355266a8ca25c56c509c5fed130eda5
Arjun <arjun.suresh@amd.com>
2026-08-08 17:46:27 +0530

    Support custom paths for tcmalloc,mimalloc

--- script/get-gperftools/customize.py
+++ script/get-gperftools/customize.py
@@ -9,6 +9,9 @@ def preprocess(i):
     env = i['env']
     state = i['state']
 
+    if is_true(env.get('MLC_TCMALLOC_LIB_PATH_PROVIDED', '')):
+        return {'return': 0}
+
     os_info = i['os_info']
 
     return {'return': 0}
@@ -21,6 +24,18 @@ def postprocess(i):
 
     os_info = i['os_info']
 
+    # Case 1: user-provided library path (path.# variation)
+    if is_true(env.get('MLC_TCMALLOC_LIB_PATH_PROVIDED', '')):
+        lib_path = env.get('MLC_TCMALLOC_LIB_PATH', '')
+        if not lib_path or not os.path.isdir(lib_path):
+            return {'return': 1, 'error': 'Provided MLC_TCMALLOC_LIB_PATH does not exist: ' + str(lib_path)}
+        env['MLC_TCMALLOC_LIB_PATH'] = lib_path
+        env['MLC_GPERFTOOLS_PATH'] = os.path.dirname(lib_path)
+        env['+LD_LIBRARY_PATH'] = [lib_path]
+        env['MLC_DEPENDENT_CACHED_PATH'] = lib_path
+        return {'return': 0}
+
+    # Case 2: source build
     lib_path = os.path.join(os.getcwd(), "install", "lib")
 
     env['+LD_LIBRARY_PATH'] = [lib_path]
--- script/get-lib-mimalloc/customize.py
+++ script/get-lib-mimalloc/customize.py
@@ -9,6 +9,9 @@ def preprocess(i):
     env = i['env']
     state = i['state']
 
+    if is_true(env.get('MLC_MIMALLOC_LIB_PATH_PROVIDED', '')):
+        return {'return': 0}
+
     os_info = i['os_info']
 
     cmake_command = f"""{env['MLC_CMAKE_BIN_WITH_PATH']} {env['MLC_MIMALLOC_SRC_PATH']} """
@@ -28,6 +31,18 @@ def postprocess(i):
 
     os_info = i['os_info']
 
+    # Case 1: user-provided library path (path.# variation)
+    if is_true(env.get('MLC_MIMALLOC_LIB_PATH_PROVIDED', '')):
+        lib_path = env.get('MLC_MIMALLOC_LIB_PATH', '')
+        if not lib_path or not os.path.isdir(lib_path):
+            return {'return': 1, 'error': 'Provided MLC_MIMALLOC_LIB_PATH does not exist: ' + str(lib_path)}
+        env['MLC_MIMALLOC_LIB_PATH'] = lib_path
+        env['MLC_MIMALLOC_PATH'] = os.path.dirname(lib_path)
+        env['+LD_LIBRARY_PATH'] = [lib_path]
+        env['MLC_DEPENDENT_CACHED_PATH'] = lib_path
+        return {'return': 0}
+
+    # Case 2: source build
     lib_path = os.path.join(os.getcwd(), "lib")
 
     env['+LD_LIBRARY_PATH'] = [lib_path]
